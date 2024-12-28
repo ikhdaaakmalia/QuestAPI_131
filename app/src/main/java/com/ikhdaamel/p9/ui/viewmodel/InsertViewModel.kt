0c8aa54a773fad98c0447aa1e1f3ead2c7032a8f
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-28 15:01:22 +0700

    membuat fungsi Mahasiswa.toInsertUiEvent

---
+++ app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,12 @@
+package com.ikhdaamel.p9.ui.viewmodel
+
+import com.ikhdaamel.p9.model.Mahasiswa
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
