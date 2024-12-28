c4c85d52ca12b2cd260c941cf4f21b7d402643e7
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-28 14:24:09 +0700

    membuat fungsi get untuk menampilkan data mahasiswa

--- app/src/main/java/com/ikhdaamel/p9/service/MahasiswaService.kt
+++ app/src/main/java/com/ikhdaamel/p9/service/MahasiswaService.kt
@@ -1,12 +1,19 @@
 package com.ikhdaamel.p9.service
 
-
+import com.ikhdaamel.p9.model.Mahasiswa
+import retrofit2.http.GET
 import retrofit2.http.Headers
-
+import retrofit2.http.Query
 
 interface MahasiswaService {
     @Headers(
         "Accept: application/json",
         "Content-Type: application.json",
     )
+    @GET("bacamahasiswa.php")
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    @GET("baca1mahasiswa.php/{nim}")
+    suspend fun getMahasiswaById(@Query("nim")nim:String):Mahasiswa
+
 }
