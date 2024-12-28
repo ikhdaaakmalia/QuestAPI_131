4a71ee54e1e53b4442b95b366ae0b61ff8127626
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-28 15:04:47 +0700

    membuat fungsi InsertUiEvent.toMhs

--- app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,15 @@ package com.ikhdaamel.p9.ui.viewmodel
 
 import com.ikhdaamel.p9.model.Mahasiswa
 
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
