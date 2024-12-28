31e1ca685362e555e0a482f200736666d34856c9
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-28 15:46:23 +0700

    membuat fungsi getMhs

--- app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/HomeViewModel.kt
@@ -4,11 +4,30 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.ikhdaamel.p9.repository.MahasiswaRepository
-
+import kotlinx.coroutines.launch
+import okio.IOException
 
 
 class HomeViewModel (private val mhs: MahasiswaRepository): ViewModel(){
     var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+    init {
+        getMhs()
+    }
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
 
 }
