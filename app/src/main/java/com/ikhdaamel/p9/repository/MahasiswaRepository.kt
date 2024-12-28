ed9a1580143bfad3e1faf51ae8a9ae054a949303
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-28 14:35:28 +0700

    membuat class NetworkMahasiswaRepository

--- app/src/main/java/com/ikhdaamel/p9/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/ikhdaamel/p9/repository/MahasiswaRepository.kt
@@ -1,7 +1,8 @@
 package com.ikhdaamel.p9.repository
 
 import com.ikhdaamel.p9.model.Mahasiswa
-
+import com.ikhdaamel.p9.service.MahasiswaService
+import okio.IOException
 
 interface MahasiswaRepository {
     suspend fun getMahasiswa(): List<Mahasiswa>
@@ -9,6 +10,37 @@ interface MahasiswaRepository {
     suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
     suspend fun deleteMahasiswa(nim: String)
     suspend fun getMahasiswaById(nim: String):Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+): MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa){
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if(!response.isSuccessful){
+                throw IOException("gagal menghapus kontak. HTTP kode: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswaById(nim)
+    }
 }
 
 
