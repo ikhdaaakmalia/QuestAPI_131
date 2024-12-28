760afd0c894666e03a3f9f54b34786a2a0de2717
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-28 15:46:47 +0700

    membuat fungsi deleteMhs

--- app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/HomeViewModel.kt
@@ -29,5 +29,15 @@ class HomeViewModel (private val mhs: MahasiswaRepository): ViewModel(){
             }
         }
     }
-
+    fun deleteMhs(nim:String) {
+        viewModelScope.launch{
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
 }
