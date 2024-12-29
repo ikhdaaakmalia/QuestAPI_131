b335a28d21e810af9448c0b6df5e5491ff18b1fc
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-29 23:19:53 +0700

    membuat Penyedia ViewModel

---
+++ app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,17 @@
+package com.ikhdaamel.p9.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.ikhdaamel.p9.MahasiswaApplication
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(MahasiswaApp().container.mahasiswaRepository) }
+        initializer { InsertViewModel(MahasiswaApp().container.mahasiswaRepository) }
+    }
+
+    fun CreationExtras.MahasiswaApp(): MahasiswaApplication =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplication)
+}
