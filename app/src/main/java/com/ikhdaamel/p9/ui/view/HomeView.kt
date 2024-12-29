dcbcaf77d711cc06e5237e6297cff14c670787b1
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-29 23:36:52 +0700

    membuat fungsi fungsi Mahasiswa Layout

--- app/src/main/java/com/ikhdaamel/p9/ui/view/HomeView.kt
+++ app/src/main/java/com/ikhdaamel/p9/ui/view/HomeView.kt
@@ -1,11 +1,15 @@
 package com.ikhdaamel.p9.ui.view
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
@@ -20,8 +24,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.ikhdaamel.p9.model.Mahasiswa
 
-
-
+@Composable
+fun MhsLayout(
+    mahasiswa: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Mahasiswa) -> Unit,
+    onDeleteClick: (Mahasiswa) -> Unit = {}
+){
+    LazyColumn (
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(mahasiswa) { mahasiswa ->
+            MhsCard(
+                mahasiswa = mahasiswa,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {onDetailClick(mahasiswa)},
+                onDeleteClick = {
+                    onDeleteClick(mahasiswa)
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun MhsCard(
