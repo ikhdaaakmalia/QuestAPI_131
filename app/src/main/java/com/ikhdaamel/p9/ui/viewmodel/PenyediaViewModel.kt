d206f5a05633a6ca6b5aec374240923dcc5eff9f
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-30 11:50:50 +0700

    menambahkan initializer

--- app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.ikhdaamel.p9.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,6 +11,8 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(MahasiswaApp().container.mahasiswaRepository) }
         initializer { InsertViewModel(MahasiswaApp().container.mahasiswaRepository) }
+        initializer { UpdateViewModel(createSavedStateHandle(), MahasiswaApp().container.mahasiswaRepository) }
+        initializer { DetailViewModel(createSavedStateHandle(), MahasiswaApp().container.mahasiswaRepository) }
     }
 
     fun CreationExtras.MahasiswaApp(): MahasiswaApplication =
