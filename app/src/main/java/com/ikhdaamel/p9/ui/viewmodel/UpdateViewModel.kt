2ff221c18c1ccaa8c33ce90e687d8b7a85dd4c5f
ikhdaaakmalia <punyaamelaja@gmail.com>
2024-12-30 11:48:50 +0700

    membuat UpdateViewModel

---
+++ app/src/main/java/com/ikhdaamel/p9/ui/viewmodel/UpdateViewModel.kt
@@ -0,0 +1,41 @@
+package com.ikhdaamel.p9.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ikhdaamel.p9.repository.MahasiswaRepository
+import com.ikhdaamel.p9.ui.view.DestinasiUpdate
+import kotlinx.coroutines.launch
+
+class UpdateViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+): ViewModel(){
+    var updateUIState by mutableStateOf(InsertUiState())
+        private set
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.nim])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = mahasiswaRepository.getMahasiswaById(_nim)
+                .toUiStateMhs()
+        }
+    }
+    fun updateInsertState(insertUiEvent: InsertUiEvent){
+        updateUIState = InsertUiState(
+            insertUiEvent = InsertUiEvent()
+        )
+    }
+    suspend fun updateMahasiswa(){
+        viewModelScope.launch {
+            try {
+                mahasiswaRepository.updateMahasiswa(_nim, updateUIState.insertUiEvent.toMhs() )
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
